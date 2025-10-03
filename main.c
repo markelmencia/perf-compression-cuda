f5ec3d5663da29811e54b391ef9321dc9b0bf69b
Markel Mencía <markel.mnc@gmail.com>
2025-10-03 12:53:33 +0200

    Create lexical order sort function

--- main.c
+++ main.c
@@ -1,26 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h> 
 
-char* get_rotation_table(char* input, int length) {
-    char *result = malloc(sizeof(char) * (length * length));
+char** get_rotation_table(char* input, int length) {
+    char **result = malloc(sizeof(char*) * length);
     for (int row = 0; row < length; row++) {
+        char* row_string = malloc(sizeof(char) * length);
         for (int col = 0; col < length; col++) {
-            result[col + (row * length)] = input[((row + col) % length)];
+            row_string[col] = input[((row + col) % length)];
         }
+        result[row] = row_string;
     }
+
     return result;
+}
+
+// Lexicographically compares two strings.
+int cmp_lex(const void* a, const void* b) {  
+    // First, void pointers a and b (which are meant to be strings) 
+    // are casted into string pointers (char**). Then, their string 
+    // values are obtained with *(char**)a and *(char**)b and are
+    // passed as arguments to strcmp.
+
+    return strcmp(*(char**)a, *(char**)b); 
+} 
+
+// Sorts the given table lexicographically.
+void sort_lexical_order(char** table, int length) {
+    qsort(table, length, sizeof(char*), cmp_lex);
 }
 
 int main() {
     // Input MUST Include a null character
-    char* input = "BANANA";
-    char* teburu = get_rotation_table(input, 7);
+    char* input = "BANANA&";
+    char** table = get_rotation_table(input, 7);
+
+    sort_lexical_order(table, 7);
     for (int i = 0; i < 7; i++) {
         for (int j = 0; j < 7; j++) {
-            printf("%c", teburu[j + (i * 7)]);
+            printf("%c", table[i][j]);
         }
+        free(table[i]);
         printf("\n");
     }
+    free(table);
 }
 
 void bwt_transform() {
